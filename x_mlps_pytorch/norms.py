d92954962541df10fb47097e883c9a735a548159
lucidrains <lucidrains@gmail.com>
2025-11-30 14:42:38 -0800

    eps for norms

--- x_mlps_pytorch/norms.py
+++ x_mlps_pytorch/norms.py
@@ -9,21 +9,25 @@ import torch.nn.functional as F
 class RMSNorm(Module):
     def __init__(
         self,
-        dim
+        dim,
+        eps = 1e-5
     ):
         super().__init__()
         self.gamma = nn.Parameter(torch.zeros(dim))
+        self.eps = eps
 
     def forward(self, x):
-        return F.rms_norm(x, x.shape[-1:]) * (self.gamma + 1)
+        return F.rms_norm(x, x.shape[-1:], self.gamma + 1, eps = self.eps)
 
 class LayerNorm(Module):
     def __init__(
         self,
-        dim
+        dim,
+        eps = 1e-5
     ):
         super().__init__()
         self.gamma = nn.Parameter(torch.zeros(dim))
+        self.eps = eps
 
     def forward(self, x):
-        return F.layer_norm(x, x.shape[-1:]) * (self.gamma + 1)
+        return F.layer_norm(x, x.shape[-1:], self.gamma + 1, eps = self.eps)
